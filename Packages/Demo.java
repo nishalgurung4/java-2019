be6d743977288e57d2211909ffadec84670d604a
Nishal Gurung <nishal.gurung4@outlook.com>
2021-12-08 19:21:43 +0545

    add packages

---
+++ Packages/Demo.java
@@ -0,0 +1,9 @@
+import np.edu.gces.*;
+class Demo
+{
+	public static void main(String[] args)
+	{
+		MyDate myDate = new MyDate();
+		myDate.display();
+	}
+}
---
+++ Packages/MyDate.java
@@ -0,0 +1,13 @@
+package np.edu.gces;
+public class MyDate
+{
+	public static void main(String[] args)
+	{
+		System.out.println("MyDate");
+	}
+
+	public void display()
+	{
+		System.out.println("Displaying....");
+	}
+}
